ab0939e06858ce51266af8b864bafa5d56ead36b
borissal1207 <borissal127@gmail.com>
2026-07-20 21:39:21 +0900

    feat: logout 연결

--- rankings/views.py
+++ rankings/views.py
@@ -4,8 +4,8 @@ from accounts.models import User
 
 
 def ranking(request):
-    users = User.objects.order_by('-total_score', 'username')
+    users = User.objects.order_by('-total_score', 'username')[:6]
     return render(request, 'rankings/ranking.html', {
         'top_users': users[:3],
-        'rest_users': users[3:],
+        'rest_users': users[3:6],
     })
